7c6afbdd196df96e64b6d6b9d2ed8a1abc2aab64
jack <8209433+jacktuck@users.noreply.github.com>
2022-02-28 23:24:09 +0000

    move stuff around to add Ubersicht

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,9 +7,13 @@ nvim -c ':PlugInstall' -c ':UpdateRemotePlugins' -c ':qall'
 
 ./iterm2/setup.sh
 
-for dir in $(fd -t d | xargs -n 1 basename); do
+for dir in $(fd -t d . stow/ | xargs -n 1 basename); do
   echo "stowing $dir";
-  stow $dir -t ~;
+ # stow -d stow -D $dir
+  stow -R -d stow $dir -t ~;
 done
+
+git clone https://github.com/kkga/nibar "Ubersicht/Ubersicht/widgets/nibar"
+stow Ubersicht -t "$HOME/Library/Application Support/"
 
 source ~/.zshrc;
