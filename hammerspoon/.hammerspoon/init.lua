cffd76ac5e416ceedb5564afb5f40198995c3c93
jack <8209433+jacktuck@users.noreply.github.com>
2022-03-05 16:38:38 +0000

    tweaks

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -2,7 +2,10 @@ stackline = require "stackline"
 stackline:init()
 
 hs.ipc.cliInstall()
+
+-- prefs
 hs.autoLaunch(true)
+hs.menuIcon(false)
 
 brew_prefix = string.gsub(hs.execute("brew --prefix", true), '[\r\n]+', '')
 
