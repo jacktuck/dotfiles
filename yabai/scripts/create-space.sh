6d602892463e43ea1a7dd86bfb9b60b8efec4c6f
jack <8209433+jacktuck@users.noreply.github.com>
2022-03-06 19:25:48 +0000

    make osascript self contained

--- yabai/scripts/create-space.sh
+++ yabai/scripts/create-space.sh
@@ -1,9 +1,13 @@
-do shell script "open -b 'com.apple.exposelauncher'"
-delay 0.5
-tell application id "com.apple.systemevents"
-    tell (every application process ¬
-        whose bundle identifier = "com.apple.dock") to ¬
-        click (button 1 of group 2 of group 1 of group 1)
+osascript <<EOF
+on run argv
+    do shell script "open -b 'com.apple.exposelauncher'"
     delay 0.5
-    key code 53 -- esc key
-end tell
+    tell application id "com.apple.systemevents"
+        tell (every application process ¬
+            whose bundle identifier = "com.apple.dock") to ¬
+            click (button 1 of group 2 of group 1 of group 1)
+        delay 0.5
+        key code 53 -- esc key
+    end tell
+end run
+EOF
