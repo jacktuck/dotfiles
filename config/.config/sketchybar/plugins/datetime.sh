e5fc64f5cae86cf849b8af7aeb0df65e5ec9b196
jack <8209433+jacktuck@users.noreply.github.com>
2022-07-01 18:06:08 +0100

    update menu bar

--- config/.config/sketchybar/plugins/datetime.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-sketchybar --set $NAME label="$(date '+%H:%M %d/%m/%Y')" 
-
--- config/.config/sketchybar/plugins/space.sh
+++ config/.config/sketchybar/plugins/space.sh
@@ -1,16 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
-args=()
-if [ "$NAME" != "space_template" ]; then
-  args+=(--set $NAME label=$NAME \
-                     icon.highlight=$SELECTED)
-fi
-
-if [ "$SELECTED" = "true" ]; then
-  args+=(--set spaces_$DID.label label=${NAME#"spaces_$DID."})
-  args+=(--set $NAME icon.background.y_offset=-12)
-else
-  args+=(--set $NAME icon.background.y_offset=-20)
-fi
-
-sketchybar -m --animate tanh 20 "${args[@]}"
+sketchybar --set $NAME background.drawing=$SELECTED
+# sketchybar --animate sin 30 --set $NAME icon.highlight=$SELECTED background.drawing=$SELECTED
