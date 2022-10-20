83c704ac09e48db9b5b83a954bec5329db3dd075
jack <8209433+jacktuck@users.noreply.github.com>
2022-10-20 14:45:45 +0100

    configure lualine

--- config/.config/nvim/lua/plugins/lualine.lua
+++ config/.config/nvim/lua/plugins/lualine.lua
@@ -4,5 +4,48 @@ require('lualine').setup {
     section_separators = '',
     component_separators = '',
     always_divide_middle = false,
+  },
+  sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {
+        {
+            'filename',
+            file_status = true, -- displays file status (readonly status, modified status)
+            path = 1, -- 0 = just filename, 1 = relative path, 2 = absolute path
+        },
+    },
+    lualine_x = {},
+    lualine_y = {},
+    lualine_z = {},
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {
+        {
+            'filename',
+            file_status = true, -- displays file status (readonly status, modified status)
+            path = 1, -- 0 = just filename, 1 = relative path, 2 = absolute path
+        },
+    },
+    lualine_x = {},
+    lualine_y = {},
+    lualine_z = {},
+  },
+  winbar = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {
+        {},
+        {
+            'filename',
+            file_status = true, -- displays file status (readonly status, modified status)
+            path = 1, -- 0 = just filename, 1 = relative path, 2 = absolute path
+        },
+    },
+    lualine_x = {},
+    lualine_y = {},
+    lualine_z = {},
   }
 }
