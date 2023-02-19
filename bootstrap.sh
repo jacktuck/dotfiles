045f13b13dc1c3d29172cee0fb1f6a1633dd677e
jack <8209433+jacktuck@users.noreply.github.com>
2023-02-19 22:31:02 +0000

    updates

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,15 +2,27 @@
 
 cd "$(dirname "${BASH_SOURCE}")";
 
-echo 'Installing/Updating Neovim plugins'
-nvim -c ':PlugInstall' -c ':UpdateRemotePlugins' -c ':qall'
+# Install brew
+echo "Installing brew"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-source ~/.zshrc;
+# Install vim-plug
+echo "Installing vim-plug"
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
+# Install brew dependencies
+brew bundle install
+
+# Stow dotfiles
 export STOW_DIRS=config,zsh,git,tmux,yabai
-
 for dir in $(echo $STOW_DIRS | sed "s/,/ /g"); do
   echo "stowing $dir";
   stow $dir -t ~;
 done
 
+source ~/.zshrc;
+
+# Install nvim dependencies
+echo 'Installing/Updating Neovim plugins'
+nvim -c ':PlugInstall' -c ':UpdateRemotePlugins' -c ':qall'
