1bf0a782eb98277351da057462be9cac52a9f2ce
jack <8209433+jacktuck@users.noreply.github.com>
2023-02-24 01:43:49 +0000

    use native lsp

---
+++ config/.config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,12 @@
+require("mason").setup()
+require("mason-lspconfig").setup {
+    ensure_installed = {
+        "tsserver",
+        "pyright",
+        "lua_ls",
+        "rust_analyzer", 
+        "docker_compose_language_service", 
+        "bashls", 
+        "gopls"
+    }
+}
