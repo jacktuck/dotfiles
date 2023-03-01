68203b0fd6c8377469df468cdee01a92bee71149
jack <8209433+jacktuck@users.noreply.github.com>
2023-03-01 22:36:37 +0000

    shrug

--- config/.config/nvim/lua/plugins/telescope.lua
+++ config/.config/nvim/lua/plugins/telescope.lua
@@ -1,32 +1,15 @@
 local builtin = require('telescope.builtin')
 
-require'telescope'.setup{
+require 'telescope'.setup {
   defaults = require('telescope.themes').get_dropdown {},
   pickers = {
     find_files = {
-      find_command = { "rg", "--files", "--hidden"},
-    }
+      find_command = { "rg", "--files", "--hidden" },
+    },
+    live_grep = {
+      additional_args = function(opts)
+        return { "--hidden" }
+      end
+    },
   }
 }
-
-_G.open_telescope = function()
-    local first_arg = vim.v.argv[2]
-    if first_arg and vim.fn.isdirectory(first_arg) == 1 then
-      if first_arg == '.' then
-        first_arg = null
-      end
-
-      builtin.find_files({
-        find_command = { "rg", "--files", "--hidden" },
-        search_dirs = {first_arg}
-      })
-    end
-end
-
-vim.api.nvim_exec([[
-augroup TelescopeOnEnter
-    autocmd!
-    autocmd VimEnter * lua open_telescope()
-augroup END
-]], false)
-
