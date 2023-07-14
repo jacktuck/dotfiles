10f01153e16b2585def4547d1f164000a49fe0fb
jack <8209433+jacktuck@users.noreply.github.com>
2023-07-14 01:47:49 +0100

    more cmp sources

--- config/.config/nvim/lua/plugins/nvim-cmp.lua
+++ config/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -50,7 +50,9 @@ cmp.setup({
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
     { name = 'nvim_lsp_signature_help' },
-    { name = 'luasnip' }, -- For luasnip users.
+    { name = 'luasnip' },
+    { name = 'tmux' },
+    { name = 'calc' },
   }, {
     { name = 'buffer' },
   })
