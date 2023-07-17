6abf026219acb923de541883d9530438a6441f5a
jack <8209433+jacktuck@users.noreply.github.com>
2023-07-17 11:21:34 +0100

    more keymaps

--- config/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ config/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -31,10 +31,6 @@ require("lspconfig").gopls.setup {
   capabilities = capabilities,
 }
 
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
 
 -- Use LspAttach autocommand to only map the following keys
 -- after the language server attaches to the current buffer
@@ -49,17 +45,15 @@ vim.api.nvim_create_autocmd('LspAttach', {
     local opts = { buffer = ev.buf }
     vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
     vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-    vim.keymap.set('n', '<space>wl', function()
-      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    end, opts)
-    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-    vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', 'lh', vim.lsp.buf.signature_help, opts)
+    vim.keymap.set({ 'n', 'v' }, 'la', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', 'lr', vim.lsp.buf.rename, opts)
+    vim.keymap.set('n', 'eo', vim.diagnostic.open_float)
+    vim.keymap.set('n', 'el', vim.diagnostic.setqflist)
+    vim.keymap.set('n', '[e', vim.diagnostic.goto_prev)
+    vim.keymap.set('n', ']e', vim.diagnostic.goto_next)
   end,
 })
 
