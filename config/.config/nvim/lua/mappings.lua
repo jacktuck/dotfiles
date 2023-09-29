66fd40e62250662d408b2e209af1351ce49424db
jack <8209433+jacktuck@users.noreply.github.com>
2023-09-29 14:54:06 +0100

    updates

--- config/.config/nvim/lua/mappings.lua
+++ config/.config/nvim/lua/mappings.lua
@@ -3,6 +3,14 @@ vim.keymap.set('n', '<leader>lc', "\"ayiwoconsole.log(\"<C-R>a:\",<C-R>a);<Esc>0
 vim.keymap.set('n', '<leader>li', "\"ayiwologger.info(\"<C-R>a:\",<C-R>a);<Esc>0", {})
 vim.keymap.set('n', '<leader>le', "\"ayiwologger.error(\"<C-R>a:\",<C-R>a);<Esc>0", {})
 
+-- Re-center verticle jumps
+vim.keymap.set('n', '<C-u>', '<C-u>zz', {})
+vim.keymap.set('n', '<C-d>', '<C-d>zz', {})
+vim.keymap.set('n', 'n', 'nzz', {})
+vim.keymap.set('n', 'N', 'Nzz', {})
+
+
+
 -- Source vimrc
 vim.keymap.set('n', '<leader><leader>r', ":source $MYVIMRC<CR>", {})
 
@@ -18,26 +26,37 @@ vim.keymap.set('n', '<leader>gd', ":Git diff<CR>", {})
 vim.keymap.set('n', '<leader>gl', ":Git l<CR>", {})
 vim.keymap.set('n', '<leader>gc', function()
 	local msg = vim.fn.input("Commit msg: ")
-	return ':Git commit -m "' .. msg .. '"<CR>'
+	if (msg ~= nil and msg ~= '') then
+		return ':Git commit -m "' .. msg .. '"<CR>'
+	end
 end, { expr = true })
+
 vim.keymap.set('n', '<leader>gw', ":Gwrite<CR>", {})
 vim.keymap.set('n', '<leader>gr', ":Gread<CR>", {})
 vim.keymap.set('n', '<leader>gb', ":Git blame<CR>", {})
 vim.keymap.set('n', '<leader>gv', ":Git show<CR>", {})
+vim.keymap.set('n', '<leader>gP', ":Git push<CR>", {})
+vim.keymap.set('n', '<leader>gp', ":Git pull<CR>", {})
 vim.keymap.set('n', '<leader>go', ":GBrowse<CR>", {})
 
 -- Telescope
-vim.keymap.set('n', '<leader>ff', ":Telescope find_files<CR>", {})
-vim.keymap.set('n', '<leader>fl', ":Telescope resume<CR>", {})
+vim.keymap.set('n', '<leader>f.', ":Telescope resume<CR>", {})
 vim.keymap.set('n', '<leader>fs', ":Telescope git_status<CR>", {})
 vim.keymap.set("n", "<leader>fe", ":Telescope file_browser path=%:p:h select_buffer=true<CR>", {})
 vim.keymap.set('n', '<leader>fb', function()
 	require('telescope.builtin').buffers({ ignore_current_buffer = true, sort_mru = true })
 end)
-vim.keymap.set('n', '<leader>fg', function()
-	local search = vim.fn.input("Grep > ")
-	if (string.len(search) > 1) then require('telescope.builtin').grep_string({ search = search }) end
-end)
+
+
+vim.keymap.set('n', '<leader>fg', ":Telescope live_grep<CR>", {})
+vim.keymap.set('n', '<leader>fG', function ()
+	return ':Telescope live_grep default_text=<C-r><C-w><CR>'
+end, { expr = true })
+
+vim.keymap.set('n', '<leader>ff', ":Telescope find_files<CR>", {})
+vim.keymap.set('n', '<leader>fF', function ()
+	return ':Telescope find_files default_text=<C-r><C-w><CR>'
+end, { expr = true })
 
 -- Quickfix lists
 vim.keymap.set('n', '[q', ":cp<CR>", {})
@@ -65,10 +84,9 @@ vim.keymap.set('n', '<Esc>', ':noh<CR>', {})
 -- Buffers
 vim.keymap.set('n', '[b', ':bprevious<CR>', {})
 vim.keymap.set('n', ']b', ':bnext<CR>', {})
-vim.keymap.set('n', 'bd', ':bd<CR>', {})
-vim.keymap.set('n', 'bd!', ':bd!<CR>', {})
-vim.keymap.set('n', 'bda', ':bufdo bd<CR>', {})
-vim.keymap.set('n', 'bda!', ':bufdo bd!<CR>', {})
+vim.keymap.set('n', '<leader>bd', ':bd<CR>', {})
+vim.keymap.set('n', '<leader>bd!', ':bd!<CR>', {})
+vim.keymap.set('n', '<leader>bd!!', ':bufdo bd!<CR>', {})
 
 vim.keymap.set('n', '<leader>f', ':lua vim.lsp.buf.format()<CR>', {})
 
