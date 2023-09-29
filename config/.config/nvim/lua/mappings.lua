37663296ddb6653f2833b2a395dc3235930d4d4e
jack <8209433+jacktuck@users.noreply.github.com>
2023-09-29 02:45:14 +0100

    check for empty commit msg

--- config/.config/nvim/lua/mappings.lua
+++ config/.config/nvim/lua/mappings.lua
@@ -18,8 +18,11 @@ vim.keymap.set('n', '<leader>gd', ":Git diff<CR>", {})
 vim.keymap.set('n', '<leader>gl', ":Git l<CR>", {})
 vim.keymap.set('n', '<leader>gc', function()
 	local msg = vim.fn.input("Commit msg: ")
-	return ':Git commit -m "' .. msg .. '"<CR>'
+	if (msg ~= nil and msg ~= '') then
+		return ':Git commit -m "' .. msg .. '"<CR>'
+	end
 end, { expr = true })
+
 vim.keymap.set('n', '<leader>gw', ":Gwrite<CR>", {})
 vim.keymap.set('n', '<leader>gr', ":Gread<CR>", {})
 vim.keymap.set('n', '<leader>gb', ":Git blame<CR>", {})
