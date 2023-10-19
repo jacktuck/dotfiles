037d52637252a3c574d1cfcb3ab482b84127f997
jack <8209433+jacktuck@users.noreply.github.com>
2023-10-19 03:16:22 +0100

    init treesitter

--- config/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ config/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -17,7 +17,6 @@ require 'nvim-treesitter.configs'.setup {
   }
 }
 
-
 require 'treesitter-context'.setup {
   enable = true,            -- Enable this plugin (Can be enabled/disabled later via commands)
   max_lines = 2,            -- How many lines the window should span. Values <= 0 mean no limit.
