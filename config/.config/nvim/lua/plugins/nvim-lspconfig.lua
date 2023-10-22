110d7a5ac48049d542e59908660a4633cd1ec613
jack <8209433+jacktuck@users.noreply.github.com>
2023-10-22 17:00:06 +0100

    format lua files

--- config/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ config/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,63 +1,74 @@
 -- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-require("lspconfig").tsserver.setup {
+require("lspconfig").tsserver.setup({
   settings = {
     completions = {
       completeFunctionCalls = true,
-    }
+    },
   },
   capabilities = capabilities,
-}
-require("lspconfig").pyright.setup {
+})
+require("lspconfig").pyright.setup({
   capabilities = capabilities,
-}
-require("lspconfig").lua_ls.setup {
+})
+require("lspconfig").lua_ls.setup({
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { "vim" },
+      },
+      workspace = {
+        checkThirdParty = false,
+      },
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+})
+
+require("lspconfig").rust_analyzer.setup({
   capabilities = capabilities,
-}
-require("lspconfig").rust_analyzer.setup {
+})
+require("lspconfig").docker_compose_language_service.setup({
   capabilities = capabilities,
-}
-require("lspconfig").docker_compose_language_service.setup {
-  capabilities = capabilities,
-}
-require("lspconfig").bashls.setup {
+})
+require("lspconfig").bashls.setup({
   capabilities = capabilities,
   filetypes = { "sh", "zsh" },
   settings = {
-    filetypes = { "sh", "zsh" }
-  }
-}
-require("lspconfig").gopls.setup {
+    filetypes = { "sh", "zsh" },
+  },
+})
+require("lspconfig").gopls.setup({
   capabilities = capabilities,
-}
-
+})
 
 -- Use LspAttach autocommand to only map the following keys
 -- after the language server attaches to the current buffer
-vim.api.nvim_create_autocmd('LspAttach', {
-  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("UserLspConfig", {}),
   callback = function(ev)
     -- Enable completion triggered by <c-x><c-o>
-    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+    vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
 
     -- Buffer local mappings.
     -- See `:help vim.lsp.*` for documentation on any of the below functions
     local opts = { buffer = ev.buf }
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-    vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, opts)
-    vim.keymap.set('n', '<leader>h', vim.lsp.buf.signature_help, opts)
-    vim.keymap.set({ 'n', 'v' }, '<leader>df', vim.lsp.buf.code_action, opts)
-    vim.keymap.set('n', 'do', vim.diagnostic.open_float)
-    vim.keymap.set('n', 'dl', vim.diagnostic.setqflist)
-    vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-    vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+    vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, opts)
+    vim.keymap.set("n", "<leader>h", vim.lsp.buf.signature_help, opts)
+    vim.keymap.set({ "n", "v" }, "<leader>df", vim.lsp.buf.code_action, opts)
+    vim.keymap.set("n", "do", vim.diagnostic.open_float)
+    vim.keymap.set("n", "dl", vim.diagnostic.setqflist)
+    vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
+    vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
   end,
 })
-
 
 vim.diagnostic.config({ signs = true, virtual_text = false })
 
@@ -65,5 +76,3 @@ vim.fn.sign_define("DiagnosticSignError", { numhl = "DiagnosticSignError" })
 vim.fn.sign_define("DiagnosticSignWarn", { numhl = "DiagnosticSignError" })
 vim.fn.sign_define("DiagnosticSignInfo", { numhl = "DiagnosticSignInfo" })
 vim.fn.sign_define("DiagnosticSignHint", { numhl = "DiagnosticSignHint" })
-
-
--- config/.config/nvim/lua/plugins/telescope.lua
+++ config/.config/nvim/lua/plugins/telescope.lua
@@ -1,68 +1,68 @@
-local builtin = require('telescope.builtin')
+local builtin = require("telescope.builtin")
 
-require 'telescope-all-recent'.setup {}
+require("telescope-all-recent").setup({})
 
-require 'telescope'.setup {
-	defaults = require('telescope.themes').get_ivy {
-		path_display = { "truncate" },
-		file_ignore_patterns = { ".git/" },
-		preview = {
-			treesitter = false
-		},
-		layout_config = {
-			height = 100,
-		},
-	},
-	extensions = {
-		file_browser = {
-			preview = {
-				ls_short = true,
-			},
-			hidden = { file_browser = true, folder_browser = false },
-			respect_gitignore = false,
-			depth = 1,
-			display_stat = false,
-			disable_devicons = true,
-		},
-	},
+require("telescope").setup({
+  defaults = require("telescope.themes").get_ivy({
+    path_display = { "truncate" },
+    file_ignore_patterns = { ".git/" },
+    preview = {
+      treesitter = false,
+    },
+    layout_config = {
+      height = 100,
+    },
+  }),
+  extensions = {
+    file_browser = {
+      preview = {
+        ls_short = true,
+      },
+      hidden = { file_browser = true, folder_browser = false },
+      respect_gitignore = false,
+      depth = 1,
+      display_stat = false,
+      disable_devicons = true,
+    },
+  },
 
-	pickers = {
-		find_files = {
-			path_display = function(opts, path)
-				local tail = require("telescope.utils").path_tail(path)
-				return string.format("%s - %s", tail, path)
-			end,
-			find_command = { "fd", "--type", "file", "--hidden" },
-			disable_devicons = true,
-		},
-		live_grep = {
-			only_sort_text = true,
-			additional_args = function(opts)
-				return { "--hidden" }
-			end,
-			disable_devicons = true
-		},
-		grep_string = {
-			only_sort_text = true,
-			additional_args = function(opts)
-				return { "--hidden" }
-			end,
-			disable_devicons = true
-		},
-		buffers = {
-			path_display = function(opts, path)
-				local tail = require("telescope.utils").path_tail(path)
-				return string.format("%s - %s", tail, path)
-			end,
-			mappings = {
-				i = {
-					["<C-d>"] = require('telescope.actions').delete_buffer
-				}
-			},
-			disable_devicons = true,
-		}
-	}
-}
+  pickers = {
+    find_files = {
+      path_display = function(opts, path)
+        local tail = require("telescope.utils").path_tail(path)
+        return string.format("%s - %s", tail, path)
+      end,
+      find_command = { "fd", "--type", "file", "--hidden" },
+      disable_devicons = true,
+    },
+    live_grep = {
+      only_sort_text = true,
+      additional_args = function(opts)
+        return { "--hidden" }
+      end,
+      disable_devicons = true,
+    },
+    grep_string = {
+      only_sort_text = true,
+      additional_args = function(opts)
+        return { "--hidden" }
+      end,
+      disable_devicons = true,
+    },
+    buffers = {
+      path_display = function(opts, path)
+        local tail = require("telescope.utils").path_tail(path)
+        return string.format("%s - %s", tail, path)
+      end,
+      mappings = {
+        i = {
+          ["<C-d>"] = require("telescope.actions").delete_buffer,
+        },
+      },
+      disable_devicons = true,
+    },
+  },
+})
 
 require("telescope").load_extension("file_browser")
 require("telescope").load_extension("harpoon")
