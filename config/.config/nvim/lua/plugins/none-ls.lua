5a631d25a7990a60ee653bdee7978f7ffddd2eda
jack <8209433+jacktuck@users.noreply.github.com>
2023-10-22 16:30:35 +0100

    mapping

--- config/.config/nvim/lua/plugins/none-ls.lua
+++ config/.config/nvim/lua/plugins/none-ls.lua
@@ -1,17 +1,34 @@
 local null_ls = require("null-ls")
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 null_ls.setup({
-    sources = {
-        null_ls.builtins.diagnostics.eslint_d.with({
-            parefer_local = "node_modules/.bin",
-            diagnostics_format = "[#{c}] #{m} (#{s})",
+  sources = {
+    null_ls.builtins.completion.spell,
+    null_ls.builtins.formatting.stylua,
+    null_ls.builtins.diagnostics.eslint_d.with({
+      parefer_local = "node_modules/.bin",
+      diagnostics_format = "[#{c}] #{m} (#{s})",
 
-        }),
-        -- Should be able to just run eslint
-        null_ls.builtins.formatting.prettierd.with({
-            prefer_local = true,
-            diagnostics_format = "[#{c}] #{m} (#{s})",
-        }),
-    },
-    debug = false
+    }),
+    null_ls.builtins.formatting.prettierd.with({
+      prefer_local = true,
+      diagnostics_format = "[#{c}] #{m} (#{s})",
+    }),
+  },
+
+  on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+          -- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
+          vim.lsp.buf.format({ async = false })
+        end,
+      })
+    end
+  end,
+  debug = false
 })
