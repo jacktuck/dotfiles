fe49b9fae14240bc3317e2b7140641d5958f0215
jack <8209433+jacktuck@users.noreply.github.com>
2023-10-25 17:08:21 +0100

    tmux confg chg and disable yaml fmt

--- config/.config/nvim/lua/plugins/none-ls.lua
+++ config/.config/nvim/lua/plugins/none-ls.lua
@@ -12,6 +12,7 @@ null_ls.setup({
     null_ls.builtins.formatting.prettierd.with({
       prefer_local = true,
       diagnostics_format = "[#{c}] #{m} (#{s})",
+      disabled_filetypes = { "yaml" },
     }),
   },
 
