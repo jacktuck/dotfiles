e79751783e34f481692a6027cbf1961645b848ca
jack <8209433+jacktuck@users.noreply.github.com>
2023-11-03 01:07:49 +0000

    borrow auto_create_dir from lazyvim

--- config/.config/nvim/init.lua
+++ config/.config/nvim/init.lua
@@ -251,4 +251,19 @@ vim.g["test#strategy"] = "neovim"
 vim.g["test#javascript#runner"] = "jest"
 vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]])
 
+-- Auto create dir when saving a file, in case some intermediate directory does not exist
+local function augroup(name)
+  return vim.api.nvim_create_augroup(name, { clear = true })
+end
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+  group = augroup("auto_create_dir"),
+  callback = function(event)
+    if event.match:match("^%w%w+://") then
+      return
+    end
+    local file = vim.loop.fs_realpath(event.match) or event.match
+    vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
+  end,
+})
+
 vim.cmd("syntax on")
