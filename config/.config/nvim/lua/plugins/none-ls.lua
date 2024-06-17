1d52ea7ab446fd612666656727cdd64f266523d5
jack <8209433+jacktuck@users.noreply.github.com>
2024-06-17 14:15:27 +0100

    update

--- config/.config/nvim/lua/plugins/none-ls.lua
+++ config/.config/nvim/lua/plugins/none-ls.lua
@@ -1,10 +1,11 @@
 local null_ls = require("null-ls")
+
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 null_ls.setup({
   sources = {
     null_ls.builtins.formatting.stylua,
-    null_ls.builtins.diagnostics.eslint_d.with({
+    require("none-ls.diagnostics.eslint_d").with({
       parefer_local = "node_modules/.bin",
       diagnostics_format = "[#{c}] #{m} (#{s})",
     }),
