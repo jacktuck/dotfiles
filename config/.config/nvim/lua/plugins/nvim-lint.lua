2705d85ca9d46e2f69e875b72fa97b7223577c2a
jack <8209433+jacktuck@users.noreply.github.com>
2025-08-11 11:32:58 +0100

    linting

--- config/.config/nvim/lua/plugins/nvim-lint.lua
+++ config/.config/nvim/lua/plugins/nvim-lint.lua
@@ -5,12 +5,8 @@ lint.linters_by_ft = {
   javascriptreact = { "eslint" },
   typescript = { "eslint" },
   typescriptreact = { "eslint" },
-
-  -- Python example
   python = { "flake8" },
-
-  -- Lua example
-  lua = { "luacheck" },
+  -- lua = { "luacheck" },
 }
 
 vim.api.nvim_create_autocmd({ "BufWritePost" }, {
