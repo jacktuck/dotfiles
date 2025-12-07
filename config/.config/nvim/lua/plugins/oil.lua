d07b3d036fcea7085ab43816fcb7b501a51ddc68
Jack Tuck <8209433+jacktuck@users.noreply.github.com>
2025-12-07 16:47:43 +0000

    oil - show hidden files

--- config/.config/nvim/lua/plugins/oil.lua
+++ config/.config/nvim/lua/plugins/oil.lua
@@ -2,7 +2,11 @@ return {
   "stevearc/oil.nvim",
   lazy = false,
   opts = function()
-    return {}
+    return {
+      view_options = {
+        show_hidden = true,
+      },
+    }
   end,
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function(_, opts)
