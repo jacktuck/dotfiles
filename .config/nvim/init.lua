378c468f4c8b4e44f372f127a9519d55a6c7a639
Jack Tuck <8209433+jacktuck@users.noreply.github.com>
2026-02-26 16:58:30 +0000

    consolidate mappings

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -43,8 +43,29 @@ vim.loader.enable()
 --Change mapleader
 vim.g.mapleader = " "
 
--- Map double leader to switch to alternate buffer
+-- Keymaps
 vim.keymap.set("n", "<leader><leader>", ":b#<CR>", { noremap = true, silent = true, desc = "Alternate buffer" })
+vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]])
+
+-- Logging
+vim.keymap.set("n", "<leader>lc", '"ayiwoconsole.log("<C-R>a:",<C-R>a);<Esc>0', { desc = "Console log current word" })
+vim.keymap.set("n", "<leader>li", '"ayiwologger.info("<C-R>a:",<C-R>a);<Esc>0', { desc = "Logger info current word" })
+vim.keymap.set("n", "<leader>le", '"ayiwologger.error("<C-R>a:",<C-R>a);<Esc>0', { desc = "Logger error current word" })
+
+-- Re-center vertical jumps
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Up half page, center" })
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Down half page, center" })
+vim.keymap.set("n", "n", "nzz", { desc = "Next match, center" })
+vim.keymap.set("n", "N", "Nzz", { desc = "Prev match, center" })
+
+-- Write file
+vim.keymap.set("n", "<leader>w", ":w<CR>", { desc = "Write file" })
+vim.keymap.set("n", "<leader>W", ":noautocmd w<CR>", { desc = "Write without formatting" })
+vim.keymap.set("n", "<leader>w!", ":w !sudo tee % > /dev/null<CR>", { desc = "Write as sudo" })
+
+-- Buffers
+vim.keymap.set("n", "[b", ":bprevious<CR>", { desc = "Previous buffer" })
+vim.keymap.set("n", "]b", ":bnext<CR>", { desc = "Next buffer" })
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
@@ -66,11 +87,8 @@ Event.mappings["User LazyFile"] = Event.mappings.LazyFile
 vim.g.sleuth_debug = 1
 require("lazy").setup("plugins")
 
-require("mappings")
-
 vim.g["test#strategy"] = "neovim"
 vim.g["test#javascript#runner"] = "jest"
-vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]])
 
 -- Auto create dir when saving a file, in case some intermediate directory does not exist
 local function augroup(name)
